aaf01871dba1b414e0608a324a55d666aa852749
mqnoy <1677058+mqnoy@users.noreply.github.com>
2019-11-26 15:58:09 +0700

    fix word case

--- system/Autoload.php
+++ system/Autoload.php
@@ -7,11 +7,10 @@ class Autoload{
     {
         
         spl_autoload_register(function($class){
-            // echo "Calling" . __FUNCTION__ . " in " . __CLASS__ . "\n<br/>";
-            echo "this var class in autooad => $class";
-            $replace_slash = str_replace("\\","/",$class);
-            $location_class = str_replace(__CLASS__,"",$replace_slash);
-            $call_class = $location_class."".__CLASS__;
+            $call_class = str_replace("\\","/",$class);
+            // $replace_slash = str_replace("\\","/",$class);
+            // $location_class = str_replace(__CLASS__,"",$replace_slash);
+            // $call_class = $location_class."".__CLASS__;
 
             include_once strtolower($call_class).".php";
         });
