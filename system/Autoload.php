c1841adc19eded38539c847ffaac28871f16f33b
mqnoy <1677058+mqnoy@users.noreply.github.com>
2019-11-26 16:41:31 +0700

    remove namspace :(

--- system/Autoload.php
+++ system/Autoload.php
@@ -5,15 +5,23 @@ class Autoload{
     function __construct()
     {
         
-        spl_autoload_register(function($class){
-	        include_once "app/controller/{$class}". '.php';
-        });
+        spl_autoload_register($this->controller_loader);
         
 
         
     }
-    function class_loader($class)
+    function controller_loader($class)
     {
-        
+        include_once "app/controller/{$class}". '.php';
+    }
+    function model_loader($class)
+    {
+        include_once "app/model/{$class}". '.php';
+    }
+    function library_loader($class){
+        include_once "app/library/{$class}". '.php';
+    }
+    function system_loader($class){
+        include_once "system/core/{$class}". '.php';
     }
 }
