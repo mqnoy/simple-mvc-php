a9243f1e319ccdbcf092514f30b28dff761f153f
mqnoy <1677058+mqnoy@users.noreply.github.com>
2019-11-26 16:05:13 +0700

    fix word case

--- system/Autoload.php
+++ system/Autoload.php
@@ -3,13 +3,29 @@ namespace System\Core;
 
 class Autoload{
 
+    //class directories
+    private $directorys = array(
+        'config/',
+        'classes/otherclasses/',
+        'classes2/',
+        'module1/classes/'
+    );
+
     function __construct()
     {
         
-        spl_autoload_register(function ($class) {
-            $file = str_replace('\\', DIRECTORY_SEPARATOR, $class).'.php';
-            if (file_exists($file)) {
-                require $file;
+        spl_autoload_register(function ($className) {
+            $className = ltrim($className, '\\');
+            $fileName = '';
+            $namespace = '';
+            if ($lastNsPos = strripos($className, '\\')) {
+                $namespace = substr($className, 0, $lastNsPos);
+                $className = substr($className, $lastNsPos + 1);
+                $fileName = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
+            }
+            $fileName = __DIR__ . DIRECTORY_SEPARATOR . $fileName . $className . '.php';
+            if (file_exists($fileName)) {
+                require $fileName;
                 return true;
             }
             return false;
